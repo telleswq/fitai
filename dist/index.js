e1a2fd92ccbd6e0ad58c42dc3ba620a863dd9314
Gabriel Telles <telleswq@gmail.com>
2026-04-06 15:13:01 -0300

    chore: add typescript and tsx setup

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+console.log("Hello, TypeScript 6.0.2 with NodeNext module system and ES2024 target!");
+export {};
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+console.log(
+  "Hello, TypeScript 6.0.2 with NodeNext module system and ES2024 target!",
+);
